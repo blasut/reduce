5984dbaa152480fe5f46af773f57052b942869f9
blasut <blasut937@gmail.com>
2018-11-03 11:27:56 +0100

    add ramda and sanctuary

---
+++ funs.js
@@ -0,0 +1,3 @@
+console.log('funs');
+
+console.log("ramda", R)
---
+++ sanc.js
@@ -0,0 +1,17 @@
+const {create, env} = require('sanctuary');
+
+console.log("funs");
+
+const S = create({
+  checkTypes: process.env.NODE_ENV !== 'production',
+  env: env,
+});
+
+
+var arr = [1,2,3]
+
+console.log(
+  S.head(arr)
+  , S.filter (S.lt (3)) ([1, 2, 3, 4, 5])
+)
+
